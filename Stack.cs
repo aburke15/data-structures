def1bba1a7de06bf7f610c6d436fec62d20fe76c
Andre Burke <aburke@Andres-MacBook-Pro.local>
2018-09-30 22:15:05 -0600

    implemented push method for the stack

--- Stack.cs
+++ Stack.cs
@@ -4,22 +4,37 @@ namespace DataStructures
 {
     public class Stack<T>
     {
-        private const int INITIAL_SIZE = 5;
-        private int _size;
-        private T[] _stack;
+        private const int INITIAL_SIZE = 2;
+        private int size;
+        private T[] stack;
 
         public Stack()
         {
-            _size = 0;
-            _stack = new T[INITIAL_SIZE];
+            size = 0;
+            stack = new T[INITIAL_SIZE];
+        }
+
+        // method that resizes the array
+        private void Resize(int newSize)
+        {
+            
+        }
+
+        // method to push an item T onto the stack
+        public void Push(T item)
+        {
+            if (size == stack.Length)
+                Resize(stack.Length * 2);
+
+            stack[size++] = item;
         }
 
         // add private method to check if stack is empty
         private bool IsEmpty() 
-            => _size == 0;
+            => size == 0;
 
         // method to return size int of stack
         public int Size() 
-            => _size;
+            => size;
     }
 }
