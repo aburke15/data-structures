7e7db7eed271f5cb59b7aff91df9bf50c6cd89d3
Andre Burke <aburke@Andres-MacBook-Pro.local>
2018-09-30 18:51:57 -0600

    added init object creation for Stack.cs and some methods.

--- Stack.cs
+++ Stack.cs
@@ -4,6 +4,22 @@ namespace DataStructures
 {
     public class Stack<T>
     {
+        private const int INITIAL_SIZE = 5;
+        private int _size;
+        private T[] _stack;
 
+        public Stack()
+        {
+            _size = 0;
+            _stack = new T[INITIAL_SIZE];
+        }
+
+        // add private method to check if stack is empty
+        private bool IsEmpty() 
+            => _size == 0;
+
+        // method to return size int of stack
+        public int Size() 
+            => _size;
     }
 }
